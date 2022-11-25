fa4db89122be808d99cfb6a837cac5ed7158736f
HarshaSalih <harshasalih2000@gmail.com>
2022-11-25 09:35:44 +0530

    commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,41 @@ import { AppComponent } from './app.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { FoodEntryComponent } from './food-entry/food-entry.component';
+import { FoodSearchComponent } from './food-search/food-search.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:UserLoginComponent
+  },
+  {
+    path:"user-registration",
+    component:UserRegisterComponent
+  },
+  {
+    path:"entry",
+    component:FoodEntryComponent
+  },
+  {
+    path:"search",
+    component:FoodSearchComponent
+  }
+  
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserLoginComponent,
     UserRegisterComponent,
-    FoodEntryComponent
+    FoodEntryComponent,
+    FoodSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
