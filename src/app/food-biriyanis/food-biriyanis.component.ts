32abf65d89980ae7934ee3e6e8ed73033605ff86
HarshaSalih <harshasalih2000@gmail.com>
2022-11-27 22:00:16 +0530

    biriyanis commits

---
+++ src/app/food-biriyanis/food-biriyanis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-biriyanis',
+  templateUrl: './food-biriyanis.component.html',
+  styleUrls: ['./food-biriyanis.component.css']
+})
+export class FoodBiriyanisComponent {
+
+}
