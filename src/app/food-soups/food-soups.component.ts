aa7dcb8a9c587f9cc1f21e6561b0500cd0490770
HarshaSalih <harshasalih2000@gmail.com>
2022-11-27 22:08:43 +0530

    soups commits

---
+++ src/app/food-soups/food-soups.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-soups',
+  templateUrl: './food-soups.component.html',
+  styleUrls: ['./food-soups.component.css']
+})
+export class FoodSoupsComponent {
+
+}
